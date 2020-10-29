92510b33ca5693e513811ba8d1a29d1406ab522a
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2020-10-29 13:44:21 -0300

    dev updates

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
           <li key={repository.id}>
             {repository.title}
 
-            <button onClick={() => handleRemoveRepository(123)}>
+            <button onClick={() => handleRemoveRepository(1)}>
               Remover
            </button>
           </li>
